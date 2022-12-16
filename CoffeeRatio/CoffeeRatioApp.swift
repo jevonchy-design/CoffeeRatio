0731c2f02d58ed22ff9b43008d83f9d7338d9224
Afropunk 4:44 <afropunk444@gmail.com>
2022-12-16 13:36:02 -0700

    Initial Commit

---
+++ CoffeeRatio/CoffeeRatioApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeRatioApp.swift
+//  CoffeeRatio
+//
+//  Created by JĒVONCHY on 12/16/22.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeRatioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoffeeRatio/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoffeeRatio
+//
+//  Created by JĒVONCHY on 12/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
